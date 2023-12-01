ebad86da552ac34a610fa037772a6289df083b1c
t-masa-works <t.masa.works@gmail.com>
2023-12-01 14:56:25 +0900

    プロダクトリストのコンポーネント作成　router-outletと紐づけ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// 起動時にindex.htmlの< app-root >に表示するコンポーネントはapp.module.ts内のbootstrapで設定
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+//main.tsはアプリを起動するためのスタートアップコード app.module.tsを読み込み、bootstrapModuleで起動→app.module.tsへ
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-
+//起動するモジュールを設定する
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
